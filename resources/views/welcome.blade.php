e7a64f4e4b32d4fb61879a507b7daaeb8b4af7ba
Turki <trki-27@hotmail.com>
2020-06-07 22:39:15 +0300

    test 10:39

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    practical training course 
+                    practical training course 1
                 </div>
 
                 <div class="links">
